a96611f5d76bd0d81da2c1752d8a6f0f07fafe7c
Alec-Wright <alecpwright@gmail.com>
2021-03-11 19:42:34 +0200

    added train_epoch and process data method to the SimpleRNN network class

--- networks.py
+++ networks.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import CoreAudioML.miscfuncs as miscfuncs
+import math
+from contextlib import nullcontext
 
 def wrapperkwargs(func, kwargs):
     return func(**kwargs)
@@ -62,6 +64,62 @@ class SimpleRNN(nn.Module):
             model_data['state_dict'] = model_state
 
         miscfuncs.json_save(model_data, file_name, direc)
+
+    # train_epoch runs one epoch of training
+    def train_epoch(self, input_data, target_data, loss_fcn, optim, bs, init_len=200, up_fr=1000):
+        # shuffle the segments at the start of the epoch
+        shuffle = torch.randperm(input_data.shape[1])
+
+        # Iterate over the batches
+        ep_loss = 0
+        for batch_i in range(math.ceil(shuffle.shape[0] / bs)):
+            # Load batch of shuffled segments
+            input_batch = input_data[:, shuffle[batch_i * bs:(batch_i + 1) * bs], :]
+            target_batch = target_data[:, shuffle[batch_i * bs:(batch_i + 1) * bs], :]
+
+            # Initialise network hidden state by processing some samples then zero the gradient buffers
+            self(input_batch[0:init_len, :, :])
+            self.zero_grad()
+
+            # Choose the starting index for processing the rest of the batch sequence, in chunks of args.up_fr
+            start_i = init_len
+            batch_loss = 0
+            # Iterate over the remaining samples in the mini batch
+            for k in range(math.ceil((input_batch.shape[0] - init_len) / up_fr)):
+                # Process input batch with neural network
+                output = self(input_batch[start_i:start_i + up_fr, :, :])
+
+                # Calculate loss and update network parameters
+                loss = loss_fcn(output, target_batch[start_i:start_i + up_fr, :, :])
+                loss.backward()
+                optim.step()
+
+                # Set the network hidden state, to detach it from the computation graph
+                self.detach_hidden()
+                self.zero_grad()
+
+                # Update the start index for the next iteration and add the loss to the batch_loss total
+                start_i += up_fr
+                batch_loss += loss
+
+            # Add the average batch loss to the epoch loss and reset the hidden states to zeros
+            ep_loss += batch_loss / (k + 1)
+            self.reset_hidden()
+        return ep_loss / (batch_i + 1)
+
+    # only proc processes a the input data and calculates the loss, optionally grad can be tracked or not
+    def process_data(self, input_data, target_data, loss_fcn, chunk, grad=False):
+        with (torch.no_grad() if grad else nullcontext()):
+            output = torch.empty_like(target_data)
+            for l in range(int(output.size()[0] / chunk)):
+                output[l * chunk:(l + 1) * chunk] = self(input_data[l * chunk:(l + 1) * chunk])
+                self.detach_hidden()
+            # If the data set doesn't divide evenly into the chunk length, process the remainder
+            if not (output.size()[0] / chunk).is_integer():
+                output[(l + 1) * chunk:-1] = self(input_data[(l + 1) * chunk:-1])
+            self.reset_hidden()
+            loss = loss_fcn(output, target_data)
+        return output, loss
 
 
 """ 
